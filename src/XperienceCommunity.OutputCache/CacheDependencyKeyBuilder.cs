fb4143320a1c6edadc987c364fa6a5b796f18366
Brandon Henricks <78803523+bluemodus-brandon@users.noreply.github.com>
2024-07-10 18:57:43 -0400

    Enhanced CacheDependencyKeyBuilder functionality
    - Added XML documentation comments to the `CacheDependencyKeyBuilder` class for better readability and understanding of the class's purpose and its methods.
    - Introduced a new method `AddPath` in the `CacheDependencyKeyBuilder` class for adding cache dependency keys based on paths, enhancing the class's flexibility.
    - Added methods `ForContentItem` and `ForWebPageItem` to set prefixes for cache dependency keys related to content and web page items, improving cache management.

--- src/XperienceCommunity.OutputCache/CacheDependencyKeyBuilder.cs
+++ src/XperienceCommunity.OutputCache/CacheDependencyKeyBuilder.cs
@@ -2,6 +2,9 @@
 
 namespace XperienceCommunity.OutputCache
 {
+    /// <summary>
+    /// Represents a builder for generating cache dependency keys.
+    /// </summary>
     public class CacheDependencyKeyBuilder
     {
         private readonly HashSet<string> _keys;
@@ -12,57 +15,108 @@ namespace XperienceCommunity.OutputCache
             _keys = new HashSet<string>();
         }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="CacheDependencyKeyBuilder"/> class.
+        /// </summary>
+        /// <returns>The new instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public static CacheDependencyKeyBuilder Create()
         {
             return new CacheDependencyKeyBuilder();
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the channel name.
+        /// </summary>
+        /// <param name="channelName">The channel name.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddChannel(string channelName)
         {
             return AddKey($"bychannel|{channelName}");
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the children of a path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddChildrenOfPath(string path)
         {
             return AddKey($"childrenofpath|{path}");
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the code name.
+        /// </summary>
+        /// <param name="codeName">The code name.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddCodeName(string codeName)
         {
             return AddKey($"byname|{codeName}");
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the content type.
+        /// </summary>
+        /// <param name="contentType">The content type.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddContentType(string contentType)
         {
             return AddKey($"bycontenttype|{contentType}");
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the GUID.
+        /// </summary>
+        /// <param name="guid">The GUID.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddGuid(Guid guid)
         {
             return AddKey($"byguid|{guid}");
         }
 
+        /// <summary>
+        /// Adds a cache dependency key based on the ID.
+        /// </summary>
+        /// <param name="id">The ID.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddId(int id)
         {
             return AddKey($"byid|{id}");
         }
-        
+
+        /// <summary>
+        /// Adds a cache dependency key based on the path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder AddPath(string path)
         {
             return AddKey($"bypath|{path}");
         }
 
+        /// <summary>
+        /// Builds the cache dependency keys.
+        /// </summary>
+        /// <returns>An array of cache dependency keys.</returns>
         public string[] Build()
         {
             return _keys.ToArray();
         }
 
+        /// <summary>
+        /// Sets the prefix for content item cache dependency keys.
+        /// </summary>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder ForContentItem()
         {
             _prefix = "contentitem";
             return this;
         }
 
+        /// <summary>
+        /// Sets the prefix for web page item cache dependency keys.
+        /// </summary>
+        /// <returns>The current instance of <see cref="CacheDependencyKeyBuilder"/>.</returns>
         public CacheDependencyKeyBuilder ForWebPageItem()
         {
             _prefix = "webpageitem";
