d9941e036d5a7d89a685770bc5d59f8ee0325ea2
Brandon Henricks <78803523+bluemodus-brandon@users.noreply.github.com>
2024-07-10 21:15:17 -0400

    Enhanced dependency key handling in HttpContext
    - Updated `using` directive to include `System.Diagnostics.CodeAnalysis` for enhanced nullability support.
    - Added remark to `AddDependencyKeys` method doc clarifying behavior when appending keys.
    - Modified `AddDependencyKeys` implementation to append new keys to existing ones instead of overwriting.
    - Annotated `GetDependencyKeys` with `[return: NotNull]` to ensure non-null return values, enhancing method contract and reliability.

--- src/XperienceCommunity.OutputCache/Extensions/HttpContextExtensions.cs
+++ src/XperienceCommunity.OutputCache/Extensions/HttpContextExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
 
 namespace XperienceCommunity.OutputCache.Extensions
 {
@@ -7,11 +8,19 @@ namespace XperienceCommunity.OutputCache.Extensions
         /// <summary>
         /// Adds the specified dependency keys to the HttpContext items.
         /// </summary>
+        /// <remarks>If the HttpContext already contains keys, new keys will be appended.</remarks>
         /// <param name="context">The HttpContext.</param>
         /// <param name="dependencyKeys">The dependency keys to add.</param>
         public static void AddDependencyKeys(this HttpContext context, string[] dependencyKeys)
         {
-            context.Items.Add(Constants.ItemKeys.DependencyKeys, dependencyKeys);
+            if (!context.Items.TryAdd(Constants.ItemKeys.DependencyKeys, dependencyKeys))
+            {
+                var existingKeys = context.Items[Constants.ItemKeys.DependencyKeys] as string[] ?? [];
+
+                var combinedKeys = existingKeys.Concat(dependencyKeys).ToArray();
+
+                context.Items[Constants.ItemKeys.DependencyKeys] = combinedKeys;
+            }
         }
 
         /// <summary>
@@ -19,6 +28,7 @@ namespace XperienceCommunity.OutputCache.Extensions
         /// </summary>
         /// <param name="context">The HttpContext.</param>
         /// <returns>The dependency keys.</returns>
+        [return: NotNull]
         public static string[] GetDependencyKeys(this HttpContext context)
         {
             return context?.Items[Constants.ItemKeys.DependencyKeys] as string[] ?? [];
